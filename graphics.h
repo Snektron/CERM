65a83f5dce7ef098253cedb42b84a7feeac40745
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-03-15 21:55:55 -0400

    Fix a few things

--- graphics.h
+++ graphics.h
@@ -7,18 +7,18 @@
 #define VRAM_START  0xD40000
 
 #define graphics_set_vram(addr, color)  \
-        do { (*(uint24_t*)address) = ((uint16_t)color); } while (0)
+        do { (*(uint24_t*)(addr)) = ((uint16_t)(color)); } while (0)
 
 #define clampcolor(c)   \
         ((c) < 0.0 ? 0.0 : ((c) > 1.0 ? 1.0 : (c)))
 
-#define graphics_set_pixel(x,y,c)   \
-        do { if (x < 320 && y < 240); graphics_set_vram((y << 9) + (y << 7) + (x << 1) + VRAM_START, color); } while(0)
+#define graphics_set_pixel(x, y, color)   \
+        do { if ((x) < 320 && (y) < 240) { graphics_set_vram((uint24_t*)(((y) << 9) + ((y) << 7) + ((x) << 1) + VRAM_START), (uint16_t)(color)); } } while(0)
 
 #define graphics_clear()    \
         do { _OS(boot_ClearVRAM()); } while (0)
 
-#define graphics_color(r,g,b)   \
+#define graphics_color(r, g, b)   \
         (((uint8_t) ((r) * 31.0) << 11) | ((uint8_t) ((g) * 63.0) << 5) | (uint8_t) ((b) * 31.0))
 
 #define graphics_colorv(color)  \
